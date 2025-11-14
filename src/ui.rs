972c78ebc98f01a2a70170b196caee3ebf5efa12
Jokezor <testifyingfor@gmail.com>
2025-11-14 20:32:41 +0100

    started on gtk

---
+++ src/ui.rs
@@ -0,0 +1,16 @@
+use glib::clone;
+
+use gtk::glib;
+use gtk::prelude::*;
+
+fn on_activate(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+    let button = gtk::Button::with_label("Hello world!");
+
+    button.connect_clicked(clone!(@weak window => move |_| window.close()));
+    window.set_child(Some(&button));
+    window.present();
+}
+
+fn main() {
+}
